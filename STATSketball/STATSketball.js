d4e9c6f56dbe540fc634326612629e55590cb656
Joe Crawley <joe.crawley10@gmail.com>
2014-02-12 19:38:21 -0800

    perfected choose team table

--- STATSketball/STATSketball.js
+++ STATSketball/STATSketball.js
@@ -1,17 +1,41 @@
 Teams = new Meteor.Collection("teams");
 
 if (Meteor.isClient) {
+  Meteor.startup(function(){
+    var chosenTeams = [];
+    Template.addTeam.teams = function(){
+      return Teams.find();
+    };
 
-  Template.addTeam.teams = function(){
-    return Teams.find();
-  };
+    var teamChoices = Teams.find().fetch();
 
-  $("tr").click(function(){
-    console.log("hi");
+    teamChoices.forEach(function(element,index,array){
+
+        $('#teams > tbody:last').append(("<tr class = 'team' id = '" + element["_id"] + "'><td><h3>" + element.name + '</h3></td></tr>'));
+
+    });
+
+    $('.team').click(function(){
+
+      var teamID = $(this).attr('id');
+      var filter = new Object();
+      filter["_id"] = teamID;
+      var team = Teams.find(filter).fetch()[0];
+
+      console.log(team);
+
+      if(teamID === 'new-team'){
+        console.log(true);
+      }
+      else{
+        $("#team-box").append("<h2>" + team.name+ "</h2>");
+      }
+
+    });
+
   });
 
 
-  console.log(Meteor.userId());
 
  
   
