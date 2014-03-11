d16b51907284cd3a24a53a77a1b66d2b8fdd782b
Joe Crawley <joe.crawley10@gmail.com>
2014-03-11 15:51:58 -0700

    added some logic for creating a new team

--- STATSketball/STATSketball.js
+++ STATSketball/STATSketball.js
@@ -1,9 +1,18 @@
 Teams = new Meteor.Collection("teams");
+Players = new Meteor.Collection("players");
 
 if (Meteor.isClient) {
   Meteor.startup(function(){
+   /* if(Meteor.userId() !== null){
+      $("#not-signed-in").css("display", "none");
+      $("#signed-in").css("display", "auto");
+    }
+    else{
+      $("#signed-in").css("display", "none");
+      $("#not-signed-in").css("display", "auto");
+    }*/
     var chosenTeams = [];
-    Template.addTeam.teams = function(){
+    Template.addPlayer.players = function(){
       return Teams.find();
     };
 
@@ -30,6 +39,42 @@ if (Meteor.isClient) {
       else{
         $("#team-box").append("<h2>" + team.name+ "</h2>");
       }
+
+    });
+
+    $('#team-info').click(function(){
+        $('#new-player-details').css('display', 'none');
+        $('#new-team-details').css('display', 'auto');
+        $('#remove-team-details').css('display', 'none');
+    });
+    $('#add-player').click(function(){
+        $('#new-player-details').css('display', 'auto');
+        $('#new-team-details').css('display', 'none');
+        $('#remove-team-details').css('display', 'none');
+    });
+
+    var pendingPlayers = [];
+    $('#add-player-button').click(function(){
+        var tempPlayer = [];
+        if($("#player-name-input").val().length <= 0 || $("#player-number-input").val().length <= 0 || $("#player-height-input").val().length <= 0){
+          $("#alert-error").html("You need to fill out all of the fields");
+          alert("Fill out all of the fields to add a player!");
+        }
+        else{
+          $("#alert-error").alert("close");
+          tempPlayer.push($("#player-name-input").val());
+          tempPlayer.push($("#player-number-input").val());
+          tempPlayer.push($("#player-height-input").val());
+
+          pendingPlayers.push(tempPlayer);
+
+          $("#player-name-input").val("");
+          $("#player-number-input").val("");
+          $("#player-height-input").val("");
+
+          console.log(pendingPlayers);
+
+        }
 
     });
 
